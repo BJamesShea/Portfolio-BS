c61e91628453d7104f3d1c3c79e1bc89e5c1cd20
Brandon Shea <brandon.shea1994@icloud.com>
2024-08-12 12:24:20 -0230

    vheader changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,12 +5,39 @@ const Projects = () => {
     <div className="projectCardContainer">
       <div className="projectCard">
         <h2 className="projectHeader">Project 1</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut blanditiis
+          nobis officiis corporis dolorum facere nemo nihil, repellendus quo
+          dolorem.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, quos!
+        </p>
+        <button>View on GitHub</button>
       </div>
       <div className="projectCard">
         <h2 className="projectHeader">Project 2</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut blanditiis
+          nobis officiis corporis dolorum facere nemo nihil, repellendus quo
+          dolorem.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, quos!
+        </p>
+        <button>View on GitHub</button>
       </div>
       <div className="projectCard">
         <h2 className="projectHeader">Project 3</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut blanditiis
+          nobis officiis corporis dolorum facere nemo nihil, repellendus quo
+          dolorem.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, quos!
+        </p>
+        <button>View on GitHub</button>
       </div>
     </div>
   );
